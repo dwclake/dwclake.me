660c4835fe50327ad758e2a455453c8cf4aa610b
dwclake <clake.dw@gmail.com>
2025-09-28 18:04:34 -0700

    added color themes and Intel Mono One font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,23 +3,28 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Personal portfolio of Devon Webb",
-  icons: {
-    icon: "/ruka/favicon/ruka-green-256x256.png",
-  },
+    title: "Portfolio",
+    description: "Personal portfolio of Devon Webb",
+    icons: {
+        icon: "/ruka/favicon/ruka-green-256x256.png",
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Intel+One+Mono:ital,wght@0,300..700;1,300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { Footer } from "components/Footer";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <Header />
-      <h1>Welcome to my Portfolio!</h1>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <Header />
+            <h1>Welcome to my Portfolio!</h1>
+            <Footer />
+        </div>
+    );
 }
