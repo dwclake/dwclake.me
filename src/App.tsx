0ea15f8f5530a6b1c11cf22f28901d1d51e22ba2
dwclake <clake.dw@gmail.com>
2025-10-01 09:16:28 -0700

    switched back to bun and react instead of Next.js

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+
+import "./App.css"
+
+export default function App() {
+  return (
+      <div>
+          <Header />
+          <h1>Welcome to my Portfolio!</h1>
+          <Footer />
+      </div>
+  )
+}
