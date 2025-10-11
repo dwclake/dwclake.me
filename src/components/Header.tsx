21a8265ab3fdd4b9491a17c2bca9329e16a95d4d
dwclake <clake.dw@gmail.com>
2025-10-10 18:43:14 -0700

    changed: css -> css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-import "./Header/Header.css"
 import logo from "~branding/ruka/svg/ruka-green-transparent.svg"
+import styles from "./Header/Header.module.css"
 
 export const Header = () => {
     return (
-        <div className="header" >
-            <img className="logo" src={logo} alt="logo" />
-            <ul className="nav">
+        <div className={styles.header} >
+            <img className={styles.logo} src={logo} alt="logo" />
+            <ul className={styles.nav}>
                 <li>Home</li>
                 <li>Code</li>
                 <li>Photography</li>
