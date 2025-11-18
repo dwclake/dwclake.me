f3fec1fcc3ea119c4ebf9a5fca1f5a8666dc54f6
dwclake <clake.dw@gmail.com>
2025-11-18 11:24:46 -0800

    feat: basic project display

---
+++ src/lib/constants.ts
@@ -0,0 +1,21 @@
+import rukaLogo from "$lib/assets/images/projects/ruka/logo.svg";
+import osLogo from "$lib/assets/images/projects/oldies-station/logo.svg";
+import eyedhdLogo from "$lib/assets/images/projects/eyeDHD/logo.png";
+
+export const projects = [
+	{
+		name: "ruka",
+		link: "",
+		logo: rukaLogo
+	},
+	{
+		name: "solecism",
+		link: "",
+		logo: osLogo
+	},
+	{
+		name: "eyedhd",
+		link: "",
+		logo: eyedhdLogo
+	}
+];
