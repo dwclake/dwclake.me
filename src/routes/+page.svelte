debdacba40b5bdd5d6ea9bbcdfd249f7cec92373
dwclake <clake.dw@gmail.com>
2025-11-23 23:01:21 -0800

    change: home styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,11 @@
 <main class="size-full p-12">
 	<!-- <div class="p-12 h-full w-full bg-linear-to-br from-light dark:from-dark from-15% dark:from-70% via-[#e2ccea] via-40% dark:via-80% to-dark dark:to-light to-90%"> -->
 		<div class="flex jusify-around items-top w-full">
-			<h1 class="text-5xl text-right font-bold md:text-7xl lg:text-8xl">
+			<h1 class="text-5xl text-right font-bold md:text-8xl lg:text-9xl">
 				DEVON<br/>JAMES<br/>WEBB
 			</h1>
 			<div class="flex ml-12">
-				<img src={portrait} class="object-fit h-50 md:h-150 lg:h-200 border border-b-32 border-mirage-950 dark:border-mirage-50 rounded-sm shadow-xl transition transform hover:scale-101 duration-300" alt="selife" />
+				<img src={portrait} class="object-fit h-50 md:h-150 lg:h-200 border-4 border-b-16 md:border-8 md:border-b-32 lg:border-16 lg:border-b-64 border-mirage-950 dark:border-mirage-50 rounded-sm md:rounded-md lg:rounded-xl shadow-xl transition transform hover:scale-101 duration-300" alt="selife" />
 			</div>
 		</div>
 	<!-- </div> -->
