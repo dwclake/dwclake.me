2b632a5727aac0fe20140125f6489582ec4a0133
dwclake <clake.dw@gmail.com>
2025-11-29 15:33:16 -0800

    change: extrapolated card and list and heading-2 styling into tailwind components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,28 +17,24 @@
 		<h1 class="text-right text-5xl font-bold lg:text-7xl xl:text-9xl">
 			DEVON<br />JAMES<br />WEBB
 		</h1>
-		<div class="flex lg:ml-12">
+		<div class="card mt-4 flex p-4 pb-16 hover:scale-101 md:mt-0 lg:ml-12">
 			<enhanced:img
 				src={portrait}
-				class="mt-4 h-auto transform rounded-xl border border-mirage-500 object-cover p-4 pb-16 shadow-md transition duration-300 hover:scale-101 hover:shadow-lg md:mt-0"
+				class="h-auto object-cover"
 				alt="selife"
 				aria-label="selfie"
 			/>
 		</div>
 	</div>
 	<div id="introduction" class="mt-6">
-		<h2 class="text-2xl font-semibold md:text-4xl xl:text-6xl">
-			Introduction
-		</h2>
+		<h2 class="heading-2">Introduction</h2>
 		<p class="mt-4 text-lg md:text-2xl">{about.introduction}</p>
 	</div>
 	<div id="projects" class="mt-6">
-		<h2 class="text-2xl font-semibold md:text-4xl xl:text-6xl">Projects</h2>
-		<ul class="flex-center mt-4 flex-wrap space-y-4 space-x-4">
+		<h2 class="heading-2">Projects</h2>
+		<ul class="list mt-4">
 			{#each projects as project}
-				<li
-					class="tranform mb-4 rounded-md border border-mirage-500 shadow-md transition duration-200 hover:scale-103 hover:shadow-lg"
-				>
+				<li class="card mb-4 p-0">
 					<a
 						href="/projects/{project.name}"
 						data-sveltekit-preload-data="tap"
@@ -59,37 +55,29 @@
 		</ul>
 	</div>
 	<div id="skills" class="mt-6">
-		<h2 class="text-2xl font-semibold md:text-4xl xl:text-6xl">Skills</h2>
-		<h4 class="mt-4">Work skills:</h4>
-		<ul class="flex flex-wrap gap-x-2 gap-y-2">
+		<h2 class="heading-2">Skills</h2>
+		<h4 class="mt-4 font-semibold">Work skills:</h4>
+		<ul class="list">
 			{#each skills.work as skill}
-				<li
-					class="transform rounded-md border border-mirage-500 p-2 text-lg shadow-md transition duration-300 hover:scale-103 hover:shadow-lg"
-				>
+				<li class="card text-lg">
 					{skill}
 				</li>
 			{/each}
 		</ul>
-		<h4 class="mt-4">Tech skills:</h4>
-		<ul class="flex flex-wrap gap-x-2 gap-y-2">
+		<h4 class="mt-4 font-semibold">Tech skills:</h4>
+		<ul class="list">
 			{#each skills.tech as skill}
-				<li
-					class="transform rounded-md border border-mirage-500 p-2 text-lg shadow-md transition duration-300 hover:scale-105 hover:shadow-lg"
-				>
+				<li class="card text-lg">
 					{skill}
 				</li>
 			{/each}
 		</ul>
 	</div>
 	<div id="education" class="mt-6">
-		<h2 class="text-2xl font-semibold md:text-4xl xl:text-6xl">
-			Education
-		</h2>
-		<ul class="mt-4 flex flex-col gap-y-2">
+		<h2 class="heading-2">Education</h2>
+		<ul class="list mt-4 flex-col">
 			{#each education as edu}
-				<li
-					class="transform rounded-md border border-mirage-500 p-4 shadow-md transition duration-300 hover:scale-103 hover:shadow-lg"
-				>
+				<li class="card p-4">
 					<h3 class="text-xl font-semibold">{edu.institution}</h3>
 					<p class="text-lg italic">
 						{#if edu.degree}
@@ -115,17 +103,13 @@
 		</ul>
 	</div>
 	<div id="experience" class="mt-6">
-		<h2 class="text-2xl font-semibold md:text-4xl xl:text-6xl">
-			Experience
-		</h2>
+		<h2 class="heading-2">Experience</h2>
 		<p class="mt-4 text-xl">Work Experience</p>
 	</div>
 	<div id="contact" class="mt-6">
-		<h2 class="text-2xl font-semibold md:text-4xl xl:text-6xl">Contact</h2>
-		<ul class="mt-4 flex flex-col gap-y-2 text-lg md:text-xl">
-			<li
-				class="transform rounded-md border border-mirage-500 p-2 shadow-md transition duration-300 hover:scale-103 hover:shadow-lg"
-			>
+		<h2 class="heading-2">Contact</h2>
+		<ul class="list mt-4 flex-col">
+			<li class="card text-lg md:text-xl">
 				<span class="font-semibold">Email:</span>
 				<a
 					class="cursor-pointer text-mirage-900 hover:text-mirage-950 hover:underline"
@@ -134,9 +118,7 @@
 					{contacts.email}
 				</a>
 			</li>
-			<li
-				class="transform rounded-md border border-mirage-500 p-2 shadow-md transition duration-300 hover:scale-103 hover:shadow-lg"
-			>
+			<li class="card text-lg md:text-xl">
 				<span class="font-semibold">University Email:</span>
 				<a
 					class="cursor-pointer text-mirage-900 hover:text-mirage-950 hover:underline"
@@ -145,9 +127,7 @@
 					{contacts.edu}
 				</a>
 			</li>
-			<li
-				class="transform rounded-md border border-mirage-500 p-2 shadow-md transition duration-300 hover:scale-103 hover:shadow-lg"
-			>
+			<li class="card text-lg md:text-xl">
 				<span class="font-semibold">GitHub:</span>
 				<a
 					class="cursor-pointer text-mirage-900 hover:text-mirage-950 hover:underline"
@@ -159,12 +139,10 @@
 		</ul>
 	</div>
 	<div id="hobbies" class="mt-6">
-		<h2 class="text-2xl font-semibold md:text-4xl xl:text-6xl">Hobbies</h2>
-		<ul class="flex-center mt-4 space-x-4">
+		<h2 class="heading-2">Hobbies</h2>
+		<ul class="list mt-4">
 			{#each hobbies as hobby}
-				<li
-					class="tranform rounded-md border border-mirage-500 p-2 shadow-md transition duration-200 hover:scale-103 hover:shadow-lg"
-				>
+				<li class="card">
 					<a
 						href="/others/{hobby.name}"
 						data-sveltekit-preload-data="tap"
